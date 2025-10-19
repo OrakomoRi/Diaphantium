b7c4594390cd5140dc189c10fb1993a6afbee1d4
OrakomoRi <OrakomoRi@gmail.com>
2025-10-19 22:22:23 +0300

    5.0.0-alpha.9
    chore: update version and improve styles
    
    - Add side padding variable for consistent styling
    - Center align items in supplies container
    - Adjust padding for various elements to use side padding variable

--- release/diaphantium.user.js
+++ release/diaphantium.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name			Diaphantium
-// @version			5.0.0-alpha.8
+// @version			5.0.0-alpha.9
 // @description		The tool created to make your life easier
 // @author			OrakomoRi
 
