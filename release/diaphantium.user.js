6dfb5e49d5a7ff5224e36b7cc4701e206affcca3
OrakomoRi <OrakomoRi@gmail.com>
2025-10-19 18:58:47 +0300

    5.0.0-alpha.7
    chore: fixed auto-build
    
    - Remove node_modules and other build artifacts to prevent them from being committed
    - Add cleanup step in the build process

--- release/diaphantium.user.js
+++ release/diaphantium.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name			Diaphantium
-// @version			5.0.0-alpha.6
+// @version			5.0.0-alpha.7
 // @description		The tool created to make your life easier
 // @author			OrakomoRi
 
