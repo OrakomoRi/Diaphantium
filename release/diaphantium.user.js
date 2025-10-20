a297bf97eef66e852bad3e7168c0713dc92d9370
OrakomoRi <OrakomoRi@gmail.com>
2025-10-20 06:14:56 +0300

    5.0.0-beta.0
    feat: introduce Version 5.0 with significant updates
    
    - Add new version entry for 5.0 with a complete rewrite and modern features
    - Include changelog image for Version 5.0
    - Update user script version to 5.0.0-beta.0
    - Add Russian localization for Version 5.0

--- release/diaphantium.user.js
+++ release/diaphantium.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name			Diaphantium
-// @version			5.0.0-alpha.12
+// @version			5.0.0-beta.0
 // @description		The tool created to make your life easier
 // @author			OrakomoRi
 
