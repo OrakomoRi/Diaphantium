9734e3736a2457bdd91a62ab638d96450b71e5b3
OrakomoRi <OrakomoRi@gmail.com>
2025-12-05 17:16:19 +0200

    5.0.1+build.7
    chore: update version number in user script
    
    - Update version number from 5.0.1+build.6 to 5.0.1+build.7
    - Adjust file path for copying built files in auto-build workflow

--- release/diaphantium.user.js
+++ release/diaphantium.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name			Diaphantium
-// @version			5.0.1+build.6
+// @version			5.0.1+build.7
 // @description		The tool created to make your life easier
 // @author			OrakomoRi
 
