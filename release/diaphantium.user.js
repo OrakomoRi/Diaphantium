ff844428122663fc0bea736905a85dfeec39fe68
OrakomoRi <OrakomoRi@gmail.com>
2025-12-12 19:28:26 +0200

    5.0.1+build.24
    chore: update version number and clean up Clicker class
    
    - Increment version number to 5.0.1+build.24
    - Remove redundant packet clicker reset logic in start method

--- release/diaphantium.user.js
+++ release/diaphantium.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name			Diaphantium
-// @version			5.0.1+build.23
+// @version			5.0.1+build.24
 // @description		The tool created to make your life easier
 // @author			OrakomoRi
 
