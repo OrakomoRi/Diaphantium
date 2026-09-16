c18d455434ecf7f2553a96222d886ba2888f605d
OrakomoRi <OrakomoRi@gmail.com>
2026-09-16 21:27:00 +0300

    5.0.2+build.18
    fix: remove size property from update toast
    
    - Eliminate 'size: small' from the toast configuration in main.ts

--- src/update-toast/main.ts
+++ src/update-toast/main.ts
@@ -17,7 +17,6 @@ const showUpdateToast: ShowUpdateToast = ({ name, version, date, duration }) => 
 		title: `${name}: ${strings.title}`,
 		text: updateText(strings, version, date),
 		theme: 'dark',
-		size: 'small',
 		position: 'top-right',
 		duration,
 		pauseOnHover: true,
